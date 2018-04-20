520f0529e335d5cbd11a3dfc9611d470f92e99c9
Jack Mittelmeier <jmittelmeier@dhcp-10-142-178-139.uawifi.arizona.edu>
2018-04-20 13:49:23 -0700

    added more

--- postpage.js
+++ postpage.js
@@ -10,18 +10,20 @@ function getPostPage(){
 
   var postTitle = document.createElement('input');
   postTitle.id = 'post_title';
-
+  postTitle.className = 'inputfield';
 
   var postDesc = document.createElement('textarea');
-
+  postDesc.className = 'inputfield';
 
   var addImageButton = document.createElement('button');
   addImageButton.value = 'Add Imamge';
   addImageButton.className = 'defbutton';
+  addImageButton.innerHTML = 'Add Image';
 
   var submitButton = document.createElement('button');
   submitButton.value = 'Create';
   submitButton.className = 'defbutton';
+  submitButton.innerHTML = 'Create';
   var title = postTitle.value;
   var desc = postDesc.value;
   //CODE FOR IMAGE NEEDS TO BE ADDED
@@ -29,11 +31,15 @@ function getPostPage(){
 
   page.appendChild(mainDiv);
   mainDiv.appendChild(heading);
+  mainDiv.innerHTML += 'Title: ';
+  mainDiv.innerHTML += '<br>';
   mainDiv.appendChild(postTitle);
   mainDiv.innerHTML += '<br><br>';
+  mainDiv.innerHTML += "Description: ";
+  mainDiv.innerHTML += '<br>';
   mainDiv.appendChild(postDesc);
   mainDiv.innerHTML += '<br><br>';
   mainDiv.appendChild(addImageButton);
-  mainDiv.appendChild(submitButotn);
+  mainDiv.appendChild(submitButton);
 
 }
