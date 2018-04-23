51e2c4a9d8009f89309244c76d7727d10cb9ab39
Jack Mittelmeier <jmittelmeier@CyVerses-MacBook-Pro-2.local>
2018-04-23 02:28:35 -0700

    added add a new listing

--- controller.php
+++ controller.php
@@ -2,10 +2,25 @@
     include('DatabaseAdaptor.php');
 
     function getSideBar(){
-        
+
     }
 
     function getFrontPage(){
-        
+
     }
+$categories = array(
+  "Jobs"=>0,
+  "Services"=>1,
+  "Cars"=>2,
+  "School Supplies"=>3,
+  "Pets"=>4,
+  "Electronics"=>5
+
+
+);
+$DB = new DatabaseAdaptor;
+if(isset($_POST['title'])){
+  //needs code for user ID
+  $DB->createListing($categories[$_POST['category']], $_POST['title'], $_POST['desc'], $_POST['location'], $_POST['price']);
+}
 ?>
