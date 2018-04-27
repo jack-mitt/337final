b7ed2a6ab8b2d6eb72e6355343ae79252effb706
millscan <millscan@gmail.com>
2018-04-27 13:45:36 -0700

    Styles

--- script.js
+++ script.js
@@ -13,9 +13,35 @@ function getTopBanner(){
     titleDiv.className='topbannertitle';
     titleDiv.innerHTML='UA SWAP';
     
+    let userDiv = document.createElement('div');
+    userDiv.className = 'dropdown';
+    userDiv.id='userpanel';
+    let userIcon = document.createElement('img');
+    userIcon.className='dropbtn';
+    userIcon.id='usericon';
+    userIcon.src = 'images/usericon.png';
+    
+    let accountLinkDiv = document.createElement('div');
+    accountLinkDiv.className = 'dropdown-content';
+    
+    let accountLink = document.createElement('a');
+    
+    accountLink.innerHTML = 'Link1';
+    accountLink.href = '#';
+    
+    let logoutLink = document.createElement('a');
+    logoutLink.innerHTML = 'Link2';
+    logoutLink.href = '#';
+    
+    accountLinkDiv.appendChild(accountLink);
+    accountLinkDiv.appendChild(logoutLink);
+        
+    userDiv.appendChild(userIcon);
+    userDiv.appendChild(accountLinkDiv);
     
     topBanner.appendChild(logoImg);
     topBanner.appendChild(titleDiv);
+    topBanner.appendChild(userDiv);
     
     page.appendChild(topBanner);
 }
