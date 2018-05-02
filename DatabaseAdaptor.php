e6ebd2418d0c8920a1f8ae3afcf1330bc8e3c484
millscan <millscan@gmail.com>
2018-05-01 20:58:37 -0700

    security in database adaptor

--- DatabaseAdaptor.php
+++ DatabaseAdaptor.php
@@ -28,7 +28,9 @@ class DatabaseAdaptor {
     }
     
     public function getChildrenOf($categoryId){
-        $stmt = $this->DB->prepare("SELECT * FROM categories where parent_id=" . $categoryId);
+        $categoryId = htmlspecialchars($categoryId);
+        $stmt = $this->DB->prepare("SELECT * FROM categories where parent_id= :categoryId");
+        $stmt->bindParam(':categoryId', $categoryId);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
@@ -40,32 +42,54 @@ class DatabaseAdaptor {
     }
 
     public function getPostsByCategory($categoryId){
-        $stmt = $this->DB->prepare("SELECT posts.id, posts.location, posts.postname, posts.price, categories.name FROM posts JOIN categories WHERE posts.category_id='" .$categoryId ."'");
+        $categoryId = htmlspecialchars($categoryId);
+        $stmt = $this->DB->prepare("SELECT posts.id, posts.location, posts.postname, posts.price, categories.name FROM posts JOIN categories WHERE posts.category_id=:categoryId");
+        $stmt->bindParam(':categoryId', $categoryId);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     
     public function getPostsByCategoryName($categoryName){
-        $stmt = $this->DB->prepare("SELECT posts.id, posts.postname, posts.location, posts.price, categories.name FROM posts JOIN categories WHERE posts.category_id=categories.id AND categories.name='" .$categoryName ."'");
+        $categoryName = htmlspecialchars($categoryId);
+        $stmt = $this->DB->prepare("SELECT posts.id, posts.postname, posts.location, posts.price, categories.name FROM posts JOIN categories WHERE posts.category_id=categories.id AND categories.name=:categoryName");
         $stmt->execute();
+        $stmt->bindParam(':categoryName', $categoryName);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);        
     }
 
     public function getPostsByUser($userId){
-        $stmt = $this->DB->prepare("SELECT posts.id, posts.postname, posts.price, posts.location, users.username FROM posts JOIN users WHERE posts.user_id='" .$userId ."'");
+        $userId = htmlspecialchars($userId);
+        $stmt = $this->DB->prepare("SELECT posts.id, posts.postname, posts.price, posts.location, users.username FROM posts JOIN users WHERE posts.user_id=:userId");
+        $stmt->bindParam(':userId', $userId);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function createListing($categoryID, $title, $desc, $location, $price){
+        $categoryId = htmlspecialchars($categoryId);
+        $title = htmlspecialchars($title);
+        $desc = htmlspecialchars($desc);
+        $location = htmlspecialchars($location);
+        $price = htmlspecialchars($price);
       //needs code for user ID  
       if(isset($_SESSION['user_id'])){
         $stmt = $this->DB->prepare("INSERT INTO posts (postname, description, location, price, category_id, user_id)
-       VALUES ('" . $title . "', '" . $desc . "', '" . $location . "', '" . $price . "', '" . $categoryID . "','". $_SESSION['user_id'] . "')");          
+       VALUES (:title, :desc, :location, :price, :categoryID , :userId)");  
+          $stmt->bindParam(':title', $title);
+          $stmt->bindParam(':desc', $desc);
+          $stmt->bindParam(':location', $location);
+          $stmt->bindParam(':price', $price);
+          $stmt->bindParam(':categoryID', $categoryID);
+          $stmt->bindParam(':userId', $_SESSION['user_id']);
       }
       else{
       $stmt = $this->DB->prepare("INSERT INTO posts (name, description, location, price, category_id)
-       VALUES ('" . $title . "', '" . $desc . "', '" . $location . "', '" . $price . "', '" . $categoryID . "')");
+       VALUES (:title, :desc, :location, :price, :categoryID)");
+          $stmt->bindParam(':title', $title);
+          $stmt->bindParam(':desc', $desc);
+          $stmt->bindParam(':location', $location);
+          $stmt->bindParam(':price', $price);
+          $stmt->bindParam(':categoryID', $categoryID);
       }
       $stmt->execute();
 
@@ -74,11 +98,17 @@ class DatabaseAdaptor {
     }
 
     public function findSearch($search, $catagory_id){
+        $search = htmlspecialchars($search);
+        $catagory_id =htmlspecialchars($catagory_id);
+        
       //echo 'here';
       if($catagory_id > 0){
-        $stmt = $this->DB->prepare("SELECT * FROM posts WHERE postname LIKE '%" . $search . "%' AND  category_id='" . $catagory_id . "'");
+        $stmt = $this->DB->prepare("SELECT * FROM posts WHERE postname LIKE '%:search%' AND  category_id=:catagory_id");
+          $stmt->bindParam(':search', $search);
+          $stmt->bindParam(':catagory_id', $catagory_id);
       } else{
-        $stmt = $this->DB->prepare("SELECT * FROM posts WHERE postname LIKE '%" . $search . "%'");
+        $stmt = $this->DB->prepare("SELECT * FROM posts WHERE postname LIKE '%:search%'");
+          $stmt->bindParam(':search', $search);
       }
       //AND posts.category_id = " . $category_id);
       $stmt->execute();
