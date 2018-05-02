2c053b373bc7ef1e97eca4c15cc273dbc04efe93
Jack Mittlemeier <jackmittelmeier@email.arizona.edu>
2018-05-01 23:46:39 -0700

    finished page on search page

--- searchpage.js
+++ searchpage.js
@@ -1,3 +1,5 @@
+
+var items = {};
 function getSearchPage(){
 
   var page = document.getElementById('pagecontainer');
@@ -48,6 +50,8 @@ function getSearchPage(){
 
   var postDiv = document.createElement('div');
   postDiv.id = 'postDiv';
+  var specPostDiv = document.createElement('div');
+  specPostDiv.id = 'specPostDiv';
 
   var searchButton = document.createElement('button');
   searchButton.innerHTML = 'Search';
@@ -61,6 +65,7 @@ function getSearchPage(){
       //console.log(catagory);
 
     //catagory = catagory.value;
+
     $.ajax({
       url : 'controller.php?search='+ search + '&catagory=' + catagory,
       type: 'get',
@@ -69,26 +74,26 @@ function getSearchPage(){
         //show listing names and prices
         var content = document.getElementById('postDiv');
         content.innerHTML = "";
-        console.log(data);
         data = JSON.parse(data);
-        console.log(data);
         for (var i = 0; i < data.length; i++){
           var aux = document.createElement('div');
-            aux.className = 'searchpost';
-            //TODO ADD LINK TO PAGE FOR A POST HERE. CALL JS FUNCTION THAT MAKES DIV
-            //VISIBLE AND ADDS STUFF TO IT
-          //var postLink = document.createElement('a');
-          //postLink.href = "javascript:createPostPage(document.getElementById(this.id))";
+          items[data[i]['id']] = data[i];
+          aux.className = 'searchpost';
           aux.innerHTML += '<h5 class="searchposttitle">' + data[i]['postname'] + '<h5>';
           aux.innerHTML += '<h5>' + data[i]['location'] +'</h5>' +  '<h5>$' + data[i]['price'] + '</h5>';
           aux.innerHTML += "<div class='searchpostthumbnaildiv'><img class='searchpostthumbnail' alt='"+data[i]['name']+"' src='images/posts/" + data[i]['id'] + ".jpg'></div>";
+          aux.id = data[i]['id'];
+          aux.onclick = function(){
+              createPostPage(this.id);
+          }
+          //console.log(items);
           content.appendChild(aux);
         }
       }
 
     })
-  }
 
+  }
 
   mainDiv.appendChild(headDiv);
   headDiv.innerHTML += '<b>Find:    </b>';
@@ -98,5 +103,69 @@ function getSearchPage(){
   //headDiv.innerHTML += '<br><br>';
   headDiv.appendChild(searchButton);
   mainDiv.appendChild(postDiv);
+  mainDiv.appendChild(specPostDiv);
 
+
+}
+
+function createPostPage(id){
+  item = items[id];
+  //console.log(id);
+  //console.log(item['id']);
+  var postList = document.getElementById('postDiv');
+  var postPage = document.getElementById('specPostDiv');
+  postPage.innerHTML = '';
+  postPage.className = 'postpage';
+  postPage.style.zIndex = 2;
+
+  var title = document.createElement('h1');
+  title.className = 'postobjtitle';
+  title.innerHTML = item['postname'];
+
+  var xButton = document.createElement('img');
+  xButton.src = 'images/xbutton.jpg'
+  xButton.className = 'xButton';
+  xButton.onclick = function(){
+    closeOpenItem();
+  }
+
+  var infoDiv = document.createElement('div');
+  infoDiv.className = 'postInfo';
+
+  var postPrice = document.createElement('h3');
+  postPrice.innerHTML = "$" + item['price'];
+
+  var postDesc = document.createElement('p');
+  postDesc.innerHTML = item['description'];
+
+  var postLoc = document.createElement('p');
+  postLoc.innerHTML = "<b>Location: </b>" + item['location'];
+
+  var postEmail = document.createElement('p');
+  postEmail.innerHTML = "<b>Contact: </b>" + item['email'];
+
+  var image = document.createElement('img')
+  image.className = 'postimage';
+  image.alt = item['name'];
+  image.src = 'images/posts/' + item['id'] + '.jpg';
+
+  postList.style.display = 'none';
+  postPage.style.display = 'block';
+  postPage.appendChild(title);
+  postPage.appendChild(image);
+  postPage.appendChild(xButton);
+  postPage.appendChild(infoDiv);
+  infoDiv.appendChild(postPrice);
+  infoDiv.appendChild(postDesc);
+  infoDiv.appendChild(postLoc);
+  infoDiv.appendChild(postEmail);
+}
+
+function closeOpenItem(){
+  var postPage = document.getElementById('specPostDiv');
+  var postList = document.getElementById('postDiv');
+
+  postPage.style.display = 'none';
+  postPage.innerHTML = "";
+  postList.style.display = 'block';
 }
